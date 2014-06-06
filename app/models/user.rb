367c0896183f3c6b4e44df00c7f06138e1c5824f
Richard <RJNY86@gmail.com>
2014-06-06 18:27:36 -0400

    will now validate email format.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_many :rounds
   validates :email, uniqueness: true
   validates :email, :hashed_password, presence: true
+  validates :email, format: { with: /[\dA-Za-z]{2,}@[\dA-Za-z]{2,}\.[a-zA-Z]{2,3}/,
+    message: "invalid e-mail" }
 end
