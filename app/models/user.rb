c3c1804ef2b928f91898b13faafe56048991d8e2
RJNY <RJNY86@gmail.com>
2014-06-06 18:31:48 -0400

    will now validate email format.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ActiveRecord::Base
     has_many :rounds
 	validates :email, uniqueness: true
 	validates :email, :hashed_password, presence: true
-	
+	validates :email, format: { with: /[\dA-Za-z]{2,}@[\dA-Za-z]{2,}\.[a-zA-Z]{2,3}/,
+    message: "invalid e-mail" }	
 	def password=(plaintext)
 		self.password_hash = BCrypt::Password.create(plaintext)
 	end
 end
+
